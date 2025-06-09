bfa3879bbe49e6668cde39a21a7fef361555c1ea
hanyong5 <hanyong5@naver.com>
2025-06-09 15:54:56 +0900

    first

--- webTemp.py
+++ webTemp.py
@@ -5,6 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from datetime import datetime
 import json
@@ -27,6 +29,12 @@ print(browser.title)
 browser.find_element(By.ID, 'query').click()
 browser.find_element(By.ID, 'query').send_keys("날씨") # 검색어 입력
 browser.find_element(By.CLASS_NAME,'btn_search').click()
+
+
+# 온도 정보가 나타날 때까지 최대 10초 대기
+WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '.temperature_text strong'))
+)
 
 
 data = browser.find_element(By.CSS_SELECTOR, '.temperature_text strong').text.replace('현재 온도', '').replace('°', '').strip()
@@ -61,7 +69,7 @@ data_dict = {
 
 try:
     # 기존 JSON 파일 읽기
-    with open('data.js', 'r', encoding='utf-8') as f:
+    with open('data.json', 'r', encoding='utf-8') as f:
         existing_data = json.load(f)
         if isinstance(existing_data, list):
             existing_data.insert(0, data_dict)
@@ -72,7 +80,7 @@ except FileNotFoundError:
     existing_data = [data_dict]
 
 # JSON 파일로 저장
-with open('data.js', 'w', encoding='utf-8') as f:
+with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(existing_data, f, ensure_ascii=False, indent=4)
 
 
